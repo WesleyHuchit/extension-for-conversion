d848406123723e7f8e626b183ac349517dff9ec2
Wesley Huchit <102929411+WesleyHuchit@users.noreply.github.com>
2025-02-13 22:58:41 -0300

    created downloadExcel

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
+import * as XLSX from "xlsx";
 import "../global.css";
 
+interface DataType {
+  plate: string
+  rows: Rows[]
+}
+
+interface Rows {
+  code: string
+  description: string
+  discount: string
+  finalPrice: string
+  operations: Operation[]
+  price: string
+  quantity: string
+  workshop: string
+}
+
+interface Operation {
+  operations: Operation2[]
+}
+
+interface Operation2 {
+  name: string
+  time: string
+}
+
 export const Popup = () => {
-
-  // const trElements = document.querySelectorAll('tr.type-impact:not(.type-resume)');
-
-  // console.log(trElements);
-
-  // useEffect(() => {
-  //   const executeScript = async () => {
-  //     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  //     if (!chrome?.tabs?.query) {
-  //       console.error("A API do Chrome não está disponível. Execute isso dentro de uma extensão.");
-  //       return;
-  //     }
-
-  //     if (tab) {
-  //       chrome.scripting.executeScript({
-  //         target: { tabId: tab.id! },
-  //         func: () => {
-  //           console.log("Hello from the popup!");
-  //         },
-  //       });
-  //     } else {
-  //       console.error("No active tab found!");
-  //     }
-  //   };
-
-  //   executeScript();
-  // }, [])
 
   const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
 
@@ -43,29 +41,119 @@ export const Popup = () => {
 
   useEffect(() => {
     (async () => {
-      const [tab] = await chrome.tabs.query({})
-      // const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-      // chrome.scripting.executeScript({
-      //   target: {
-      //       tabId: tab.id
-      //   },
-      //   function: () => {},
-      // });
+      if (tab) {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id! },
+          func: () => {
 
-      console.log(tab);
+
+            function validateBrazilianLicensePlate(plate: string) {
+              const oldPattern = /^[A-Z]{3}-\d{4}$/;
+              const mercosulPattern = /^[A-Z]{3}\d[A-Z]\d{2}$/;
+              return oldPattern.test(plate) || mercosulPattern.test(plate) ? plate : false;
+            }
+
+
+            const data: Rows[] = [];
+
+            const trElements = document.querySelectorAll('tr.type-impact:not(.type-resume)');
+
+            trElements.forEach(tr => {
+              const tdElements = tr.querySelectorAll('td');
+
+              const row: Rows = {
+                operations: [],
+                quantity: tdElements[1].innerText,
+                code: tdElements[2].innerText,
+                description: tdElements[3].innerText,
+                workshop: tdElements[4].innerText,
+                price: tdElements[5].innerText,
+                discount: tdElements[6].innerText,
+                finalPrice: tdElements[7].innerText
+              };
+
+              tdElements.forEach((td, index) => {
+
+                if (index === 0) {
+                  const operations = td.querySelectorAll('span.button_circle_op_report')
+                  const operationsArray = Array.from(operations).map(operation => operation instanceof HTMLElement ? operation.innerText : '');
+
+                  const opTimeReport = td.querySelectorAll('span.op_time_report');
+                  const opTimeReportArray = Array.from(opTimeReport).map(opTime => opTime instanceof HTMLElement ? opTime.innerText : '');
+
+
+                  const operationsT: Operation2[] = []
+
+                  operationsArray.forEach((operation, index) => {
+                    operationsT.push({
+                      name: operation,
+                      time: opTimeReportArray[index]
+                    })
+                  })
+
+                  row.operations.push({
+                    operations: operationsT
+                  })
+                }
+              })
+
+              data.push(row);
+
+            })
+
+            const elements = document.querySelectorAll("*:not(script):not(style)");
+
+            let foundPlates: string[] = [];
+
+            elements.forEach(element => {
+              if (element.childNodes.length) {
+                element.childNodes.forEach(node => {
+                  if (node.nodeType === Node.TEXT_NODE) {
+                    const plate = validateBrazilianLicensePlate(node.textContent || '');
+                    if (plate) {
+                      foundPlates.push(plate);
+                      return
+                    }
+                    return
+                  }
+                  return
+                });
+              }
+            });
+
+            const dataObject: DataType = {
+              plate: foundPlates[0],
+              rows: data
+            }
+
+            console.log(dataObject)
+
+            chrome.storage.local.set({ extractedData: dataObject }, () => {
+              console.log("Data saved!");
+            });
+
+          },
+        });
+      }
     })()
   }, [tabs]);
 
+  function downloadExcel() {
+    chrome.storage.local.get(['extractedData'], (result) => {
+      const data = result.extractedData as DataType;
+
+      const ws = XLSX.utils.json_to_sheet(data.rows);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+      XLSX.writeFile(wb, 'data.xlsx');
+    });
+  }
 
   return (
     <div className="text-5xl p-10 font-extrabold">
-      <div>This is your popup.</div>
-      <ul>
-        {tabs.map((tab) => (
-          <li key={tab.id}>{tab.title}</li>
-        ))}
-      </ul>
+      <button onClick={() => downloadExcel()} >Excel</button>      
     </div>
   );
 };
