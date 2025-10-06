f695f1a494954f367839eff528458bffb9b3b73b
Wesley Huchit <wesleyhuchit@gmail.com>
2025-10-06 20:44:20 -0300

    feat: implement PDF download functionality using axios in useDownload hook

--- src/chrome-extension/hooks/useDownload.tsx
+++ src/chrome-extension/hooks/useDownload.tsx
@@ -1,56 +1,33 @@
+import { useState } from "react";
 import { DataType } from "../../dtos/DataType";
+import axios from "axios";
 // import * as XLSX from "xlsx";
 
 export function useDownload() {
 
-    // const [data, setData] = useState<DataType | null>(null);
+    const [data, setData] = useState<DataType | null>(null);
 
-    function downloadExcel(data: DataType) {
+  async function handleDownload() {
+    console.log('data before downloadExcel', data);
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/extension/service-order`, data, {
+        responseType: "blob",
+      });
 
-        console.log('Downloading Excel with data:', data);
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const url = window.URL.createObjectURL(blob);
 
-        // const formattedRows = data.rows.map(row => {
-        //     const operationsObj: Record<string, string> = {};
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "ordem_servico.pdf"; // nome do arquivo
+      a.click();
 
-        //     row.operations.forEach((op, index) => {
-        //         operationsObj[`Operation ${index + 1} Name`] = op.name;
-        //         operationsObj[`Operation ${index + 1} Time`] = op.time;
-        //     });
-
-        //     const priceFormatted = parseFloat(row.price.replace(/[^0-9,]/g, ""))
-        //     const finalPriceFormatted = parseFloat(row.finalPrice.replace(/[^0-9,]/g, ""));
-
-        //     // var notCalc = false
-        //     var priceDifference = 0
-
-        //     if (isNaN(priceFormatted) || isNaN(finalPriceFormatted)) {
-        //         // notCalc = true
-        //     } else {
-        //         priceDifference = priceFormatted - finalPriceFormatted
-        //     }
-
-        //     return {
-        //         ...operationsObj,
-        //         quantity: row.quantity,
-        //         code: row.code,
-        //         description: row.description,
-        //         workshop: row.workshop,
-        //         price: row.price,
-        //         discount: row.discount,
-        //         finalPrice: row.finalPrice,
-        //         priceDifference: priceDifference,
-        //     };
-
-        // });
-
-        // const ws = XLSX.utils.json_to_sheet(formattedRows);
-        // const wb = XLSX.utils.book_new();
-        // XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-        // XLSX.writeFile(wb, 'data.xlsx');
-
-        // console.log('Downloading Excel with data:', data);
-
+      // Limpa o objeto após o download
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Erro ao gerar PDF", error);
     }
+  };
 
     // useEffect(() => {
     //     chrome.storage.local.get(['extractedData'], (result) => {
@@ -99,7 +76,9 @@ export function useDownload() {
     // }, [data])
 
     return {
-        downloadExcel
+        handleDownload,
+        data,
+        setData,
     }
 
 }
--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -9,7 +9,7 @@ import { useDownload } from "../hooks/useDownload";
 export function Popup() {
 
   const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
-  const { downloadExcel } = useDownload();
+  const { handleDownload, setData } = useDownload();
 
   useEffect(() => {
     if (chrome.tabs) {
@@ -20,106 +20,13 @@ export function Popup() {
   }, []);
 
   useEffect(() => {
-    // (async () => {
-    //   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    //   if (!tab) return;
-
-    //   chrome.scripting.executeScript({
-    //     target: {
-    //       tabId: tab.id!
-    //     },
-    //     func: () => {
-
-    //       const rows: Rows[] = [];
-
-    //       const trElements = document.querySelectorAll('tr.type-impact:not(.type-resume)');
-
-    //       trElements.forEach(tr => {
-    //         const tdElements = tr.querySelectorAll('td');
-
-    //         const row: Rows = {
-    //           operations: [],
-    //           quantity: tdElements[1].innerText,
-    //           code: tdElements[2].innerText,
-    //           description: tdElements[3].innerText,
-    //           workshop: tdElements[4].innerText,
-    //           price: tdElements[5].innerText,
-    //           discount: tdElements[6].innerText,
-    //           finalPrice: tdElements[7].innerText
-    //         };
-
-    //         tdElements.forEach((td, index) => {
-
-    //           if (index === 0) {
-    //             const operations = td.querySelectorAll('span.button_circle_op_report')
-    //             const operationsArray = Array.from(operations).map(operation => operation instanceof HTMLElement ? operation.innerText : '');
-
-    //             const opTimeReport = td.querySelectorAll('span.op_time_report');
-    //             const opTimeReportArray = Array.from(opTimeReport).map(opTime => opTime instanceof HTMLElement ? opTime.innerText : '');
-
-
-    //             const operationsT: Operation[] = []
-
-    //             operationsArray.forEach((operation, index) => {
-    //               operationsT.push({
-    //                 name: operation,
-    //                 time: opTimeReportArray[index]
-    //               })
-    //             })
-
-    //             row.operations.push(
-    //               ...operationsT
-    //             )
-    //           }
-    //         })
-
-    //         rows.push(row);
-    //       })
-
-    //       // console.log('rows', rows)
-
-    //       const elements = document.querySelectorAll("*:not(script):not(style)");
-
-    //       let foundPlates: string[] = [];
-
-    //       elements.forEach(element => {
-    //         if (element.childNodes.length) {
-    //           element.childNodes.forEach(node => {
-    //             if (node.nodeType === Node.TEXT_NODE) {
-    //               const plate = validateBrazilianLicensePlate(node.textContent || '');
-    //               if (plate) {
-    //                 foundPlates.push(plate);
-    //                 return
-    //               }
-    //               return
-    //             }
-    //             return
-    //           });
-    //         }
-    //       });
-
-    //       const dataObject: DataType = {
-    //         plate: foundPlates[0],
-    //         rows: rows
-    //       }
-
-    //       // console.log('dataObject', dataObject);
-    //       chrome.runtime.sendMessage({ action: 'extractedData', data: dataObject });
-    //       // setData(dataObject);
-
-
-    //       // chrome.storage.local.set({ extractedData: dataObject }, () => {
-    //       //   console.log("Data saved!");
-    //       // });
-
-    //     },
-    //   });
-
-    // })()
-    // console.log("Tabs:", tabs);
     setTabs(tabs);
   }, [tabs]);
+
+  useEffect(() => {
+    test()
+  }, [tabs])
+
 
   async function test() {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -232,7 +139,9 @@ export function Popup() {
     const listener = (message: any) => {
       if (message.action === 'extractedData') {
         const data = (message.data);
-        downloadExcel(data);
+        console.log('Received data in popup:', data);
+        setData(data);
+        // downloadExcel(data);
       }
     };
 
@@ -242,27 +151,6 @@ export function Popup() {
       chrome.runtime.onMessage.removeListener(listener);
     };
   }, []);
-
-    const handleDownload = async () => {
-      const response = await fetch("http://localhost:3333/extension/service-order", {
-        method: "POST",
-      });
-
-      console.log('response', response);
-
-      if (!response.ok) throw new Error("Erro ao gerar PDF");
-
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = "ordem_servico.pdf"; // nome do arquivo
-      a.click();
-
-      // Limpa o objeto após o download
-      window.URL.revokeObjectURL(url);
-    };
 
 
   return (
